3dc6740ccac51faa5f89c4cc42090cda6de3a8ba
ichikawaryouya0719 <sinkikariudop@yahoo.co.jp>
2016-09-06 06:49:08 +0000

    ゴール判定、State判定の大部分を追加 （各員の作業待ち 終わり次第追加してください）

---
+++ runnink/runnink/FIN.cpp
@@ -0,0 +1,34 @@
+
+
+void FIN(void)
+{
+	/*CUSTOMVERTEX Front_FIN[4]
+	{
+		{ X,f. X.f, 1,f.1.f, 0xFFFFFFFF X.f, X.f, },
+		{ X,f. X.f, 1,f.1.f, 0xFFFFFFFF X.f, X.f, },
+		{ X,f. X.f, 1,f.1.f, 0xFFFFFFFF X.f, X.f, },
+		{ X,f. X.f, 1,f.1.f, 0xFFFFFFFF X.f, X.f, }
+	};
+
+	
+	CUSTOMVERTEX Back_FIN[4]
+	{
+		{ X, f.X.f, 1, f.1.f, 0xFFFFFFFF X.f, X.f, },
+		{ X, f.X.f, 1, f.1.f, 0xFFFFFFFF X.f, X.f, },
+		{ X, f.X.f, 1, f.1.f, 0xFFFFFFFF X.f, X.f, },
+		{ X, f.X.f, 1, f.1.f, 0xFFFFFFFF X.f, X.f, }
+	};
+
+	CUSTOMVERTEX SHINING_POINT[4]
+	{
+		{ X, f.X.f, 1, f.1.f, 0xFFFFFFFF X.f, X.f, },
+		{ X, f.X.f, 1, f.1.f, 0xFFFFFFFF X.f, X.f, },
+		{ X, f.X.f, 1, f.1.f, 0xFFFFFFFF X.f, X.f, },
+		{ X, f.X.f, 1, f.1.f, 0xFFFFFFFF X.f, X.f, }
+	};*/
+
+	/*if (SHINING_POINT_HITFLAG == true)
+	{
+		//ÉNÉäÉAâÊñ Ç…çsÇ≠
+	};*/
+};
---
+++ runnink/runnink/State.cpp
@@ -0,0 +1,18 @@
+#include"UI.h"
+
+/*
+void PlayerLEGState(void)
+{
+	if (rightsize == INK_S)
+	{
+		//小ジャンプ
+	}
+	if (rightsize == INK_M)
+	{
+		//中ジャンプ
+	}
+	if (rightsize == INK_L)
+	{
+		//大ジャンプ
+	}
+}*/
